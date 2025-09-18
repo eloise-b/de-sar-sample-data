9bf042d2ff920d4f6d969093da1a52dd5aeb5ed5
Caitlin Adams <25995927+caitlinadams@users.noreply.github.com>
2025-09-18 10:11:19 +0930

    Update speckle filter code to use nanvar

--- speckle_filters.py
+++ speckle_filters.py
@@ -3,45 +3,54 @@ from scipy.ndimage import uniform_filter
 import xarray as xr
 
 
-# Adapted from https://stackoverflow.com/questions/39785970/speckle-lee-filter-in-python
 def lee_filter(img, size):
     """
-    Applies the Lee filter to reduce speckle noise in an image.
+    Apply a Lee filter to reduce speckle noise in an individual image.
 
-    Parameters:
-    img (ndarray): Input image to be filtered.
-    size (int): Size of the uniform filter window.
+    Adapted from https://stackoverflow.com/questions/39785970/speckle-lee-filter-in-python
 
-    Returns:
-    ndarray: The filtered image.
+    Parameters
+    ----------
+    img : numpy.ndarray
+        Input image to be filtered.
+    size : int
+        Size of the uniform filter window.
+
+    Returns
+    -------
+    numpy.ndarray
+        The filtered image.
     """
     img_mean = uniform_filter(img, size)
     img_sqr_mean = uniform_filter(img**2, size)
     img_variance = img_sqr_mean - img_mean**2
 
-    overall_variance = np.var(img)
+    overall_variance = np.nanvar(img)
 
     img_weights = img_variance / (img_variance + overall_variance)
     img_output = img_mean + img_weights * (img - img_mean)
     return img_output
 
 
-# Define a function to apply the Lee filter to a DataArray
 def apply_lee_filter(data_array, size=7):
     """
-    Applies the Lee filter to the provided DataArray.
+    Apply a Lee filter to each observation in a provided xarray.DataArray.
 
-    Parameters:
-    data_array (xarray.DataArray): The data array to be filtered.
-    size (int): Size of the uniform filter window. Default is 7.
+    Parameters
+    ----------
+    data_array : xarray.DataArray
+        The data array to be filtered.
+    size : int, optional
+        Size of the uniform filter window.
 
-    Returns:
-    xarray.DataArray: The filtered data array.
+    Returns
+    -------
+    xarray.DataArray
+        The filtered data array.
     """
-    data_array_filled = data_array.fillna(0)
     filtered_data = xr.apply_ufunc(
         lee_filter,
-        data_array_filled,
+        data_array,
         kwargs={"size": size},
         input_core_dims=[["y", "x"]],
         output_core_dims=[["y", "x"]],
@@ -49,5 +58,6 @@ def apply_lee_filter(data_array, size=7):
         vectorize=True,
         dask="parallelized",
         output_dtypes=[data_array.dtype],
+        keep_attrs=True,
     )
     return filtered_data
